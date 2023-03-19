150bb577f6308191a5634f933aff05271d85ab24
aompe <aompenpitcha98@gmail.com>
2023-03-20 01:24:30 +0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ import os
 from PIL import Image
 
 import torch
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,send_file
 
 app = Flask(__name__)
 
@@ -27,10 +27,25 @@ def predict():
         results = model([img])
 
         results.render()  # updates results.imgs with boxes and labels
-        results.save(save_dir="static/")
-        return redirect("static/image0.jpg")
+        results.save(save_dir="static/detect")
+        return redirect("/result")
 
-    return render_template("index.html")
+    return render_template("index1.html")
+
+@app.route('/return-files', methods=['GET'])
+def return_file():
+    obj = request.args.get('obj')
+    loc = os.path.join("runs/detect", obj)
+    print(loc)
+    try:
+        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
+        # return send_from_directory(loc, obj)
+    except Exception as e:
+        return str(e)
+    
+@app.route('/result')
+def result():
+    return render_template("result.html")
 
 
 if __name__ == "__main__":
@@ -38,6 +53,8 @@ if __name__ == "__main__":
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
 
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)  # force_reload = recache latest code
+    model = torch.hub.load('Ultralytics/yolov5', 'custom','best', force_reload=True,trust_repo=True)  # force_reload = recache latest code
     model.eval()
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
+
+
